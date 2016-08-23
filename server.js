0e75bf114eaf722ffdd6c7183f4721944e3c0512
Sharigar <alex.akulich@gmail.com>
2016-08-23 08:09:45 +0000

    refactoring is needed

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-var express = require('express');
+var http = require('http');
+
+var server = new http.Server(function(req, res) {
+  console.log(req.headers);
+  var ipaddress, lang, software;
+  var head = req.headers;
+  lang = head["accept-language"].match(/([^,]*),/)[1];
+  software = head["user-agent"].match(/\(([^()]*)\)/)[1];
+  res.end(JSON.stringify({ "ipaddress": head["x-forwarded-for"],"language": lang,"software":software }));
+});
+
+server.listen(8080);
+
+/*var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -7,5 +20,5 @@ app.get('/', function (req, res) {
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
-});
+});*/
 
